acd1db904f7d9b6e53622cf48c1ce0e1a984d944
unknown <zeinebbenmeriem2@gmail.com>
2022-12-07 16:51:47 +0100

    create and style header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="../public/images/troll-face.png" alt="header logo" />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
