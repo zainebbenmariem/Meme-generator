bad8d3d9a965224a33f4bb2e01f585250dd3ee1b
unknown <zeinebbenmeriem2@gmail.com>
2022-12-15 23:34:41 +0100

    get memes from API

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import memesData from "../../memesData";
 
 export default function Meme() {
@@ -7,12 +7,17 @@ export default function Meme() {
     bottomText:"",
     randomImage:"http://i.imgflip.com/1bij.jpg"
 })
-const [allMemeImages, setAllMemeImages] = useState(memesData)
+const [allMemes, setAllMemes] = useState([])
+
+useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
+},[])
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes 
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme =>({
             ...prevMeme,
             randomImage: url
